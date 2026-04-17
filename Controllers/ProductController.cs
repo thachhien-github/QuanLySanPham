139f3bec2b5eeb0f41577ebb3506528f7cd2fba0
tuhoc.cc <htbin2806@gmail.com>
2026-04-17 11:52:57 +0700

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuanLySanPham.Models;
+
+namespace QuanLySanPham.Controllers
+{
+    public class ProductController : Controller
+    {
+        private static List<Product> products = new List<Product>();
+
+        // HIỂN THỊ DANH SÁCH
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+
+        // FORM CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // XỬ LÝ CREATE
+        [HttpPost]
+        public IActionResult Create(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                // Auto tăng ID
+                p.Id = products.Count + 1;
+
+                products.Add(p);
+                return RedirectToAction("Index");
+            }
+
+            return View(p);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null) return NotFound();
+            return View(product);
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuanLySanPham.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Tên không được để trống")]
+        public string Name { get; set; }
+
+        [Range(1, 1000000, ErrorMessage = "Giá phải > 0")]
+        public double Price { get; set; }
+    }
+}
